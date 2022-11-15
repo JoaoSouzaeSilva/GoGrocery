25ef244db4018b4b683e9bdec25d4b06818e21e0
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2022-11-15 10:57:05 +0000

    fixed the layout on the about page, now just need to fix title display

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,38 +1,69 @@
-import { IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonContent, IonPage, IonToolbar } from "@ionic/react";
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonPage,
+  IonRow,
+  IonToolbar,
+} from "@ionic/react";
 import headerImg from "../images/header.png";
 
-import "./About.css"
+import "./About.css";
 
 const About = () => {
-    return(
-        <IonPage>
-            <IonToolbar>
-                <img className="img" slot="start" src={headerImg} />
-                <div className="img_text" slot="start">have it your way</div>
-                <IonButtons slot="end">
-                    <IonButton shape="round" className="back">Back</IonButton>
-                </IonButtons>
-            </IonToolbar>
-            <IonContent>
-                <IonCard className="card1">
-                    <IonCardHeader>
-                        <h1 className="card1_title">The reason you need GoGrocery</h1>
-                    </IonCardHeader>
-                </IonCard>
-                <IonCard className="card2">
-                    <IonCardHeader>
-                        <h1 className="card2_title">The product</h1>
-                    </IonCardHeader>                   
-                </IonCard>
-                <IonCard className="card3">
-                    <IonCardHeader>
-                        <IonCardTitle className="card3_title">The team</IonCardTitle>
-                    </IonCardHeader>
-                </IonCard>
-
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonToolbar>
+        <img className="img" slot="start" src={headerImg} />
+        <div className="img_text" slot="start">
+          have it your way
+        </div>
+        <IonButtons slot="end">
+          <IonButton shape="round" className="back">
+            Back
+          </IonButton>
+        </IonButtons>
+      </IonToolbar>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonCard className="card1">
+                <IonCardHeader>
+                  <div className="card_title">
+                    <h1>
+                      The reason you need GoGrocery
+                    </h1>
+                  </div>
+                </IonCardHeader>
+              </IonCard>
+            </IonCol>
+            <IonCol>
+              <IonCard className="card2">
+                <IonCardHeader>
+                  <div className="card_title">
+                    <h1>The product</h1>
+                  </div>
+                </IonCardHeader>
+              </IonCard>
+              <IonCard className="card3">
+                <IonCardHeader>
+                  <div className="card_title">
+                    <h1>The team</h1>
+                  </div>
+                </IonCardHeader>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default About;
