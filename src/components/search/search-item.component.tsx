b3cda3ac9d8a7790ba8878f6665a0c3c02a87cce
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2022-11-25 23:58:14 +0000

    Search bar fully operational

--- src/components/search/search-item.component.tsx
+++ src/components/search/search-item.component.tsx
@@ -10,20 +10,23 @@ const SearchItem = ({
 }: any) => {
   return (
     <>
-      <div
-        className="search-item"
-        onClick={() => {
-          onSelectItem();
-          onSetProductInfo(itemName, itemImg, itemPrice);
-          onProductSelect();
-        }}
-      >
-        <div className="search-item-name">{itemName}</div>
-        <img
-          src={process.env.PUBLIC_URL + "/img/" + itemImg}
-          className="search-item-image"
-        />
-      </div>
+      {itemName !== "none" && (
+        <div
+          className="search-item"
+          onClick={() => {
+            onSelectItem();
+            onSetProductInfo(itemName, itemImg, itemPrice);
+            onProductSelect();
+          }}
+        >
+          <div className="search-item-name">{itemName}</div>
+          <img
+            src={process.env.PUBLIC_URL + "/img/" + itemImg}
+            className="search-item-image"
+          />
+        </div>
+      )}
+      {itemName === "none" && <div className="no-items">There are no items with the given name...</div>}
     </>
   );
 };
