89384c48b72a9c0bc974bfe675933206eeef2bbf
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2022-11-27 23:16:20 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/GoGrocery">
       <App />
     </BrowserRouter>
   </React.StrictMode>
