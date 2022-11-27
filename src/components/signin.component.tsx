ee93f26ea28194418c3cce6f209d109d94ffeb82
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2022-11-27 00:50:34 +0000

    Update signin.component.tsx

--- src/components/signin.component.tsx
+++ src/components/signin.component.tsx
@@ -5,16 +5,17 @@ import {
   IonInput,
   IonItem,
   IonLabel,
-  IonList
+  IonList,
 } from "@ionic/react";
 import { eye, eyeOff } from "ionicons/icons";
 import { useState } from "react";
 
-
 const SignInComponent = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("Something went wrong. Please try again later.");
+  const [message, setMessage] = useState(
+    "Something went wrong. Please try again later."
+  );
   const [showToast, setShowToast] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [passwordIcon, setPasswordIcon] = useState(eye);
@@ -39,12 +40,13 @@ const SignInComponent = () => {
   };
 
   async function checkEmail() {
-    if (localStorage.getItem(email) == null) return true;
+    if (localStorage.getItem(email) !== null) return true;
     else return false;
   }
 
   function checkPassword() {
-    if (password != JSON.parse(localStorage.getItem(email)!).password) return false;
+    if (password != JSON.parse(localStorage.getItem(email)!).password)
+      return false;
     else return true;
   }
 
@@ -58,68 +60,72 @@ const SignInComponent = () => {
           password: password,
         };
         localStorage.setItem(email, JSON.stringify(user));
-      }
-      else {
-        setEmailError(true);
-      }
+      } else setPasswordError(true);
+    } else setEmailError(true);
   }
-    else {
-  setPasswordError(true);
-}
-  };
 
+  return (
+    <>
+      <IonList className="sign-in-list">
+        <IonItem fill="outline" className="sign-in-label">
+          <IonLabel className="labelfix" position="floating">
+            Email
+          </IonLabel>
+          <IonInput
+            type="email"
+            value={email}
+            placeholder="email@email.com"
+            onIonChange={(event: any) => setEmail(event.detail.value)}
+          />
+        </IonItem>
+        <IonItem fill="outline" className="sign-in-label">
+          <IonLabel background-color={"danger"} position="floating">
+            Password
+          </IonLabel>
+          <IonInput
+            type={showPassword ? "text" : "password"}
+            value={password}
+            placeholder="***********"
+            onIonChange={(event: any) => setPassword(event.detail.value)}
+          />
+          <IonButton
+            className="eye-btn eye-color"
+            slot="end"
+            fill="clear"
+            onClick={() => {
+              togglePassword();
+              changeIcon();
+            }}
+          >
+            <IonIcon icon={passwordIcon} slot="icon-only"></IonIcon>
+          </IonButton>
+        </IonItem>
+      </IonList>
 
-return (
-  <>
-    <IonList className="sign-in-list">
-      <IonItem fill="outline" className="sign-in-label">
-        <IonLabel className="labelfix" position="floating">Email</IonLabel>
-        <IonInput
-          type="email"
-          value={email}
-          placeholder="email@email.com"
-          onIonChange={(event: any) => setEmail(event.detail.value)}
-        />
-      </IonItem>
-      <IonItem fill="outline" className="sign-in-label">
-        <IonLabel background-color={"danger"} position="floating">Password</IonLabel>
-        <IonInput
-          type={showPassword ? "text" : "password"}
-          value={password}
-          placeholder="***********"
-          onIonChange={(event: any) => setPassword(event.detail.value)}
-        />
+      <IonButton className="forgotPWD" size="small" fill="clear">
+        Having trouble signing in?
+      </IonButton>
+
+      <div className="btn-container">
         <IonButton
-          className="eye-btn eye-color"
-          slot="end"
-          fill="clear"
-          onClick={() => {
-            togglePassword();
-            changeIcon();
-          }}
+          onClick={() => handleSignIn()}
+          expand="block"
+          fill="solid"
+          className="ion-buttons-signin ion-item-email-pwd"
         >
-          <IonIcon icon={passwordIcon} slot="icon-only"></IonIcon>
+          Sign In
         </IonButton>
-      </IonItem>
-    </IonList>
-
-    <IonButton
-      className="forgotPWD"
-      size="small"
-      fill="clear"
-    >
-      Having trouble signing in?
-    </IonButton>
-
-    <div className="btn-container">
-      <IonButton  onClick={() => handleSignIn()} expand="block" fill="solid" className="ion-buttons-signin ion-item-email-pwd">
-        Sign In
-      </IonButton>
-      <IonButton expand="block" fill="clear" class="ion-text-wrap" className="btn-signup ion-item-email-pwd" routerLink="/signup">
-        Don't have an account? Create one <strong>here</strong>.
-      </IonButton>
-    </div>
-    <IonAlert
+        <IonButton
+          expand="block"
+          fill="clear"
+          class="ion-text-wrap"
+          className="btn-signup ion-item-email-pwd"
+          routerLink="/signup"
+        >
+          Don't have an account? Create one <strong>here</strong>.
+        </IonButton>
+      </div>
+      <IonAlert
         isOpen={passwordError}
         onDidDismiss={() => setPasswordError(false)}
         header={"Oh no!"}
@@ -133,8 +139,8 @@ return (
         message={emailMessage}
         buttons={["OK"]}
       />
-  </>
-);
+    </>
+  );
 };
 
 export default SignInComponent;
